ac4acbde024be3ebe0deb9acfadadd15bffe5d40
Christopher Vollick <0@psycoti.ca>
2016-03-03 22:01:36 -0500

    Initial Commit
    Currently it's normal android and java, but this is the minimal project.
    
    Doesn't do anything, but does build and run.
    It's all downhill from here.

---
+++ src/main/java/ca/psycoti/reddit/HelloActivity.java
@@ -0,0 +1,22 @@
+package ca.psycoti.reddit;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HelloActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.front);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        textView.setText("Hello world!");
+    }
+
+}
