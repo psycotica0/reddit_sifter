2b067d3e4d7db9f2c51648ecbe1afbde4061f507
Christopher Vollick <0@psycoti.ca>
2016-03-13 09:51:32 -0400

    Load on Loading Item
    I've gotten rid of the stupid text, and now I have a pretty loading item.
    When I am asked to bind the loading view, I take that as the time to load more
    stuff.
    
    I think it actually works pretty well, the only issue being that right now the
    way I'm removing the loading entry it causing it to look like the end of the
    list, because it's removing it and scrolling down to consume that space.

--- src/main/kotlin/ca/psycoti/reddit/HelloActivity.kt
+++ src/main/kotlin/ca/psycoti/reddit/HelloActivity.kt
@@ -20,10 +20,9 @@ import rx.subjects.PublishSubject
 
 import android.content.Intent
 import android.net.Uri
+import android.widget.Toast
 
 import android.support.percent.PercentFrameLayout
-
-import com.jakewharton.rxbinding.view.clicks
 
 import ca.psycoti.reddit.network.HotService
 
@@ -42,9 +41,6 @@ open class HelloActivity : Activity() {
     entries = findViewById(R.id.list_view) as RecyclerView
     entries.layoutManager = LinearLayoutManager(this)
 
-    val textView = findViewById(R.id.text_view) as TextView
-    textView.setText("Hello Kotlin!")
-    textView.clicks().subscribe({listing.loadMore()})
     entries.setAdapter(adapter)
 
     adapter.clickedItems.subscribe {
@@ -53,6 +49,8 @@ open class HelloActivity : Activity() {
       intent.putExtra(ItemViewActivity.URL, it.url)
       startActivity(intent)
     }
+
+    adapter.loadingSignal.subscribe { listing.loadMore() }
 
     val metrics = DisplayMetrics()
 
@@ -63,7 +61,12 @@ open class HelloActivity : Activity() {
 
     listing.diff.subscribe(
       {diff ->
+        val oldLast = adapter.getItemCount() - 1
         adapter.items = listing.entries
+
+        // Get rid of the loading spinner
+        adapter.notifyItemRemoved(oldLast)
+
         diff.items.forEach({
           when(it) {
             is Listing.Insert -> {
@@ -74,42 +77,63 @@ open class HelloActivity : Activity() {
             }
           }
         })
+
+        // Put the loading spinner back at the end
+        adapter.notifyItemInserted(adapter.getItemCount() - 1)
       },
-      {err -> textView.setText(err.toString())}
+      {err -> Toast.makeText(this, err.toString(), Toast.LENGTH_LONG).show()}
     )
-    listing.loadMore()
   }
 
-  class EntryAdapter(val context : Context, var items : List<Entry> = ArrayList()): RecyclerView.Adapter<EntryAdapter.EntryViewHolder>() {
-    override fun getItemCount() = items.count()
+  class EntryAdapter(val context : Context, var items : List<Entry> = ArrayList()): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun getItemCount() = items.count() + 1
     var width: Int = 0
     val clickedItems = PublishSubject.create<Entry>()
+    val loadingSignal = PublishSubject.create<Unit>()
+    enum class ViewType {ITEM, LOADING}
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EntryViewHolder {
-      val view = LayoutInflater.from(context).inflate(R.layout.item, parent, false)
-      return EntryViewHolder(view)
+    override fun getItemViewType(position: Int)  = if (position == items.count())
+      ViewType.LOADING.ordinal
+      else ViewType.ITEM.ordinal
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+      when(ViewType.values()[viewType]) {
+        ViewType.ITEM -> {
+          val view = LayoutInflater.from(context).inflate(R.layout.item, parent, false)
+          return EntryViewHolder(view)
+        }
+        ViewType.LOADING -> {
+          val view = LayoutInflater.from(context).inflate(R.layout.loading, parent, false)
+          return LoadingViewHolder(view)
+        }
+      }
     }
 
-    override fun onBindViewHolder(holder: EntryViewHolder, position: Int) {
-      val (title, subreddit, url, imageSet) = items[position]
-      holder.view.setOnClickListener {
-        clickedItems.onNext(items[position])
-      }
-      holder.titleView.text = title
-      holder.subredditView.text = subreddit
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+      when(holder) {
+        is LoadingViewHolder -> {loadingSignal.onNext(Unit)}
+        is EntryViewHolder -> {
+          val (title, subreddit, url, imageSet) = items[position]
+          holder.view.setOnClickListener {
+            clickedItems.onNext(items[position])
+          }
+          holder.titleView.text = title
+          holder.subredditView.text = subreddit
 
-      val img = imageSet?.biggestImage(width)
+          val img = imageSet?.biggestImage(width)
 
-      if (img == null || img.url.isBlank()) {
-        holder.thumbnail.setImageDrawable(null)
-        holder.thumbnail.visibility = View.GONE
-      } else {
-        holder.thumbnail.visibility = View.VISIBLE
-        val pParams = holder.thumbnail.layoutParams as? PercentFrameLayout.LayoutParams
-        if (pParams != null) {
-          pParams.percentLayoutInfo.aspectRatio =  img.width.toFloat() / img.height.toFloat()
+          if (img == null || img.url.isBlank()) {
+            holder.thumbnail.setImageDrawable(null)
+            holder.thumbnail.visibility = View.GONE
+          } else {
+            holder.thumbnail.visibility = View.VISIBLE
+            val pParams = holder.thumbnail.layoutParams as? PercentFrameLayout.LayoutParams
+            if (pParams != null) {
+              pParams.percentLayoutInfo.aspectRatio =  img.width.toFloat() / img.height.toFloat()
+            }
+            Picasso.with(context).load(img.url).into(holder.thumbnail)
+          }
         }
-        Picasso.with(context).load(img.url).into(holder.thumbnail)
       }
     }
 
@@ -118,5 +142,7 @@ open class HelloActivity : Activity() {
       val subredditView = view.findViewById(R.id.subreddit) as TextView
       val thumbnail = view.findViewById(R.id.thumbnail) as ImageView
     }
+
+    class LoadingViewHolder(val view: View) : RecyclerView.ViewHolder(view)
   }
 }
