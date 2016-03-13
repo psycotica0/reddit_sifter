a77ca752dced58b8e658dffa5e3aa2bd79a4fd23
Christopher Vollick <0@psycoti.ca>
2016-03-13 08:08:15 -0400

    Off By One Error
    I believe this was causing the crashes I was seeing on paging sometimes.
    The index in that setup was the position a new element would be at, but in a
    move we aren't actually changing the number of elements, so we should have been
    moving it to (idx - 1).
    
    I think the reason it only broke some of the time is that sometimes did the
    wrong thing, but no one could see it, and it only broke when the last time was
    a move, at which point it walked off the end.

--- src/main/kotlin/ca/psycoti/reddit/models/Listing.kt
+++ src/main/kotlin/ca/psycoti/reddit/models/Listing.kt
@@ -54,14 +54,14 @@ data class Listing(val kind: String, val entries: MutableList<Entry>, var after:
         if (pos == -1) {
           // New item
           this.entries.add(item)
-          l("New " + idx + 1)
+          l("New " + idx)
           return Pair(idx + 1, ops + Insert(idx, item))
         } else {
           // Move
-          l("Move " + pos + " to "+ idx)
+          l("Move " + pos + " to "+ (idx - 1))
           this.entries.removeAt(pos)
           this.entries.add(item)
-          return Pair(idx, ops + Move(pos, idx))
+          return Pair(idx, ops + Move(pos, idx - 1))
         }
       }
     )
